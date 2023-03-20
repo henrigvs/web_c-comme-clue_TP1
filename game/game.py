3cc0e25b28a1d75400f6734927f1a3528d4b64af
henrigevenois <henri.gevenois@gmail.com>
2023-03-20 23:35:45 +0100

    Enigma API => OK User API => OK Game with session => OK Game.html OK

--- game/game.py
+++ game/game.py
@@ -6,32 +6,51 @@ from game.JSONConverter import JSONConverter
 gameBP = Blueprint('game', __name__)
 currentEnigma = 1
 
+def getEnigmasJSON():
+    enigmas_response = requests.get("http://localhost:5000/enigmas/")
+    enigmas = JSONConverter.getEnigmasFromJSON(enigmas_response.json())
+    session['enigmas'] = enigmas
+    return enigmas
+
+def goToNextEnigma():
+    return render_template('enigmas/game.html'
+                           , not_correct=False
+                           , current_enigma=currentEnigma
+                           , enigma=session['current_enigma']['description']
+                           , hint=session['current_enigma']['hint']
+                           , solution=session['current_enigma']['solution'])
+
+def goToSameEnigma():
+    return render_template('enigmas/game.html'
+                           , not_correct=True
+                           , current_enigma=currentEnigma
+                           , enigma=session['current_enigma']['description']
+                           , hint=session['current_enigma']['hint']
+                           , solution=session['current_enigma']['solution'])
 
 @gameBP.route('/', methods=['GET', 'POST'])
 def game():
     global currentEnigma
-    if 'enigmas' not in session:
-        enigmas_response = requests.get("http://localhost:5000/enigmas/")
-        enigmas = JSONConverter.getEnigmasFromJSON(enigmas_response.json())
-        session['enigmas'] = enigmas
-    enigmas = session['enigmas']
     if request.method == 'POST':
         answer = request.form.get('answer').strip().lower()
         enigma = session['current_enigma']
+
         if answer == enigma['solution'].lower():
             currentEnigma += 1
-            if currentEnigma >= len(enigmas):
+            enigmas = getEnigmasJSON()
+            if currentEnigma-1 >= len(enigmas):
+                currentEnigma = 1
                 return render_template("enigmas/game_completed.html")
-            session['current_enigma'] = enigmas[currentEnigma-1]
-            return redirect(url_for('game.game'))
+
+            else:
+                enigmas = getEnigmasJSON()
+                session['current_enigma'] = enigmas[currentEnigma - 1]
+                return goToNextEnigma()
+
         else:
-            return render_template('enigmas/game.html', enigma=session['current_enigma']
-                                   , hint=session['current_enigma']['hint']
-                                   , solution=session['current_enigma']['solution'])
+            return goToSameEnigma()
+
     else:
+        enigmas = getEnigmasJSON()
         session['current_enigma'] = enigmas[currentEnigma-1]
-        return render_template('enigmas/game.html'
-                               , current_enigma=currentEnigma
-                               , enigma=session['current_enigma']['description']
-                               , hint=session['current_enigma']['hint']
-                               , solution=session['current_enigma']['solution'])
+        return goToNextEnigma()
--- static/js/game.js
+++ static/js/game.js
@@ -1,48 +1,13 @@
-let currentEnigmaIndex = 1;
-
-function fetchEnigmaById(id) {
-  fetch(`/enigmas/${id}`)
-    .then((response) => response.json())
-    .then((data) => {
-      displayEnigma(data);
-    })
-    .catch((error) => {
-      console.error(`Error fetching enigma with ID ${id}:`, error);
+document.getElementById('sweetalert').addEventListener('click', function (){
+    var hint = this.getAttribute('hint')
+    Swal.fire({
+        customClass: {
+            popup: 'popup'
+        },
+        title: 'Hint',
+        text: hint,
+        confirmButtonClass: 'swal2-confirm button-cool',
+        confirmButtonText: 'Cool',
+        buttonsStyling: false,
     });
-}
-
-function displayEnigma(enigma) {
-  const enigmaDescription = document.getElementById("enigma-description");
-  const enigmaHint = document.getElementById("enigma-hint");
-
-  enigmaDescription.textContent = enigma.description;
-  enigmaHint.textContent = enigma.hint;
-}
-
-function checkAnswer() {
-  const userAnswerInput = document.getElementById("user-answer");
-  const userAnswer = userAnswerInput.value.trim().toLowerCase();
-
-  fetch(`/enigmas/${currentEnigmaIndex}`)
-    .then((response) => response.json())
-    .then((data) => {
-      if (userAnswer === data.solution.toLowerCase()) {
-        alert("Correct!");
-        currentEnigmaIndex++;
-        fetchEnigmaById(currentEnigmaIndex);
-      } else {
-        alert("Incorrect! Try again.");
-      }
-      userAnswerInput.value = ""; // Clear the input field
-    })
-    .catch((error) => {
-      console.error(`Error fetching enigma with ID ${currentEnigmaIndex}:`, error);
-    });
-}
-
-// Fetch the first enigma when the page loads
-fetchEnigmaById(currentEnigmaIndex);
-
-// Attach the checkAnswer function to the button
-const checkAnswerButton = document.getElementById("check-answer-button");
-checkAnswerButton.addEventListener("click", checkAnswer);
+})
